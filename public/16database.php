314ffca4c52f21cd57bbfa50e7e06937a8daa62e
khannb <838278270@qq.com>
2020-03-23 23:24:51 +0800

    删除了不必要的文件

--- public/16database.php
+++
@@ -1,7 +0,0 @@
-<?php
-    header('Content-type:text/html;charset=utf-8');
-    //连接认证
-    $link=mysqli_connect('localhost:3306','root','123456') or die('数据库失败');
-    //选择数据库
-    mysqli_query($link,'use a05');
-
